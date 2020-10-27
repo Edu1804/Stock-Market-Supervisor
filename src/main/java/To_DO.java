7c13e413779d2cd1a65011791ec840e1290df222
Edu1804 <33988445+Edu1804@users.noreply.github.com>
2020-10-27 12:47:46 +0100

    Add new tasks to the file 'to_DO'

--- src/main/java/To_DO.java
+++ src/main/java/To_DO.java
@@ -2,10 +2,19 @@ public class To_DO {
     /*Esta clase solo se ha creado para llevar una lista de lo que hay que hacer:
     1)Comentar el código de la clase principal (done)
     2)Terminar la función ActualizarDatos para que vuelva a calcular el PER, P/S y P/B (done)
-    3)Refinar la función Puntuación y hacerla más útil
+    3)Refinar la función Puntuación y hacerla más útil (done)
     4)Ver vídeos tutoriales de gradle de youtube (historial)
-    5)Pensar tareas nueva semana
+    5)Pensar tareas nueva semana (done)
     6)Arreglar problemas del menú principal (done)
+    7)Hacer pruebas de todo
+     */
 
+    /*Nuevas tareas:
+    1)Leer sobre text mining
+    2)Incorporar las dependencias a gradle
+    3)Hacer pruebas simples
+    4)Incorporar las dependencias al programa principal
+    5)Empezar a realizar pequeñas pruebas en el programa principal
+    6)Ser capaz de obtener un sólo elemento de los sec filings
      */
 }
