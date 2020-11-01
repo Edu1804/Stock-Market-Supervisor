a578dd6d5bcddb174412c464d3b78ca6b4fc988a
Edu1804 <jXv|l3`qKC/?1PVrIl8NSW3#gTCtr>
2020-11-01 23:02:25 +0100

    list updated

--- src/main/java/To_DO.java
+++ src/main/java/To_DO.java
@@ -3,19 +3,19 @@ public class To_DO {
     1)Comentar el código de la clase principal (done)
     2)Terminar la función ActualizarDatos para que vuelva a calcular el PER, P/S y P/B (done)
     3)Refinar la función Puntuación y hacerla más útil (done)
-    4)Ver vídeos tutoriales de gradle de youtube (historial)
+    4)Ver vídeos tutoriales de gradle de youtube (historial) (done)
     5)Pensar tareas nueva semana (done)
     6)Arreglar problemas del menú principal (done)
     7)Hacer pruebas de todo
-    8) Empezar programa CSV: pruebas básicas
+    8) Empezar programa CSV: pruebas básicas (done)
      */
 
     /*Nuevas tareas:
     1)Leer sobre text mining
-    2)Incorporar las dependencias a gradle
+    2)Incorporar las dependencias a gradle (done)
     3)Hacer pruebas simples
-    4)Incorporar las dependencias al programa principal
-    5)Empezar a realizar pequeñas pruebas en el programa principal
+    4)Incorporar las dependencias al programa principal (done)
+    5)Empezar a realizar pequeñas pruebas en el programa principal (done)
     6)Ser capaz de obtener un sólo elemento de los sec filings
      */
 }
