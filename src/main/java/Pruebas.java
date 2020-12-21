6a0a4482ad1da7e646435152f01ce98de918edaf
Edu1804 <edusj1804@protonmail.com>
2020-12-21 23:00:16 +0100

    Add a class "Pruebas" to do tests

--- src/main/java/Pruebas.java
+++ src/main/java/Pruebas.java
@@ -1,38 +1,33 @@
 import java.util.Scanner;
 
-public class Pruebas {
+    public class Pruebas {
+        public static void main(String[] args){
 
+            System.out.println(numeroAcciones2());
 
-
-
-    public static void main(String[] args){
-
-        /*
-        private int valor2;
-
-
-
-        public boolean comprobarValorPositivo2(float valor2){
-            //We assume that the value is positive at the beginning
-            boolean positive2=true;
-            if(valor2<0){
-                positive2=false;
-            }
-            return positive2;
         }
 
-        public static float numeroAcciones2(){
+        private static float numeroAcciones2() {
             Scanner entrada = new Scanner(System.in);
             System.out.println("Introduzca el numero de acciones diluidas de la empresa: ");
-            float acciones2=0;
-            acciones2=entrada.nextFloat();
+            float acciones2 = 0;
+            acciones2 = entrada.nextFloat();
             entrada.nextLine();
-            if(!comprobarValorPositivo2(acciones2)){
+            if (!comprobarValorPositivo2(acciones2)) {
                 System.out.println("Error, el número de acciones debe de ser positivo");
-                numeroAcciones2();
+                acciones2=numeroAcciones2();
             }
             return acciones2;
         }
+
+        public static boolean comprobarValorPositivo2(float valor2){
+            //We assume that the value is positive at the beginning
+            boolean positive2 = true;
+            if (valor2 < 0) {
+                positive2 = false;
+            }
+            return positive2;
+        }
     }
 
-}
+
